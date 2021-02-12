a6dcfd99f79cae69f51f736af1daf4b18337efe0
Surya <suryanmenon@ieee.org>
2021-02-12 17:09:08 +0530

    Slight modification to improve time complexity.

--- bubbleSort.c
+++ bubbleSort.c
@@ -4,16 +4,18 @@
 int arr[MAX];
 
 void bubbleSort(int size){
-    int i, j, temp;
-    for(i=0;i<size;i++){
-        for(j=0;j<size-1;j++){
-            if(arr[j]>arr[j+1]){
-                temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
+   int i, j, temp, flag = 0;
+   for(i=0;i<size;i++){
+       for(j=0;j<size-1-i;j++){
+           if(arr[j]>arr[j+1]){
+               temp = arr[j+1];
+               arr[j+1] = arr[j];
+               arr[j] = temp;
+               flag = 1;
+           }
+           if(flag == 0) break;
+       }
+   }
     printf("Array elements are: ");
     for(i=0;i<size;i++){
         printf("%d ",arr[i]);
