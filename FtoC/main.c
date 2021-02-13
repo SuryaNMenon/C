9e1a5372ecacc1064191e4baf110d477f26f5f9b
Surya <suryanmenon@ieee.org>
2021-02-13 18:07:42 +0530

    Moved misc. pgms to Other Programs.

--- FtoC/main.c
+++
@@ -1,7 +0,0 @@
-//This is to print a 4 line poem ‘First Fig’, by Edna Millay.
-#include<stdio.h>
-int main(void)
-{
-    printf("My candle burns at both ends;\nIt will not last the night;\nBut ah, my foes, and oh, my friends \nIt gives a lovely light.");
-    return 0;
-}
--- milestokm/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("The Passionate Shepherd to His Love\nCome live with me and be my love,\n"
-           "And we will all the pleasures prove,\n"
-           "That Valleys, groves, hills, and fields,\n"
-           "Woods, or steepy mountain yields.\nCHRISTOPHER MARLOWE");
-    return 0;
-}
