29790d4e2155d45aa7f117b434e4c3b18f9efb9a
Surya <suryanmenon@ieee.org>
2021-02-26 10:18:43 +0530

    Program to convert infix to postfix expression.

---
+++ Data Structures/Stack/infixPostfix.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+#define MAX 100
+char stack[MAX];
+int top = -1;
+
+void Push(char x){
+    if(top == MAX-1){
+        printf("Stack Overflow \n");
+        exit(1);
+    }
+    stack[++top] = x;
+}
+
+char Pop(){
+    if(top == -1){
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    return stack[top--];
+}
+
+int priority(char x){
+    if(x == '(') return 0;
+    if(x == '+' || x == '-') return 1;
+    if (x == '*' || x == '/') return 2;
+    return 0;
+}
+
+void main(){
+    char exp[MAX];
+    printf("Enter expression: ");
+    gets(exp);
+    char *e = exp;
+    char x;
+    while(*e != '\0'){
+        if(isalnum(*e))
+            printf("%c ", *e);
+        else if(*e == '(')
+            Push(*e);
+        else if(*e == ')'){
+            while((x=Pop())!='(')
+                printf("%c ",x);
+        }
+        else{
+            while(priority(stack[top])>=priority(*e))
+                printf("%c " ,Pop());
+            Push(*e);
+        }
+        e++;
+    }
+    while(top != -1){
+        printf("%c ", Pop());
+    }
+}
